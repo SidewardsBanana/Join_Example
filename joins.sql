069e8831b50c736ec27f20b3d8d723216c4f09c0
Sidewards Banana <jlxfallon@gmail.com>
2023-03-03 11:01:48 -0500

    Created Table Named customers

--- joins.sql
+++ joins.sql
@@ -1,2 +1,8 @@
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS orders;
+
+CREATE TABLE customers (
+    customer_id int NOT NULL,
+    first_name varchar(30),
+    PRIMARY KEY(customer_id)
+);
